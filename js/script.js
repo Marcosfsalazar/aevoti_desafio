1ec42c52b854d38c7012136eede908f89c40be7b
Marcos Salazar <Marcos1010521@gmail.com>
2021-01-27 19:50:59 -0400

    finished, style and request

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const MY_API_KEY = '';
+const MY_API_KEY = '4672216151ad9a4c58f7ba56d78140d5';
 
 const getWeather = (city) =>{
     let url = `http://api.weatherstack.com/current?access_key=${MY_API_KEY}&query=${city}`
@@ -9,22 +9,21 @@ const getWeather = (city) =>{
         if(request.readyState == 4 && request.status == 200){
             let txtJson = request.responseText;
             let objJson = JSON.parse(txtJson);
-
+            
+            if(objJson.success != null){
+                document.getElementById('city-weather').value = 'Cidade não encontrada!'
+            }
             //vars
             let temp = objJson.current.temperature;
             let time = objJson.location.localtime.split(" ");
             let data = time[0].split('-')
-            console.log(data)
 
-            document.getElementById('city-name').value = objJson.location.name;
+            document.getElementById('city-name').value = objJson.location.name +'/'+objJson.location.country;
             document.getElementById('city-temp').value = `${temp}C°`;
             document.getElementById('city-weather').value = objJson.current.weather_descriptions[0];
             document.getElementById('city-time').value = time[1];
-            document.getElementById('city-day').value = `${data[2]}/${data[1]}/${data[0]}`;    
-
-            console.log(objJson);
-            console.log(objJson.current.temperature)
-        }else if(request.readyState == 4 && request.status == 400){
+            document.getElementById('city-day').value = `${data[2]}/${data[1]}/${data[0]}`; 
+            document.getElementById('span-alert').innerHTML = ''   
 
         }
     }
@@ -37,7 +36,8 @@ let getSubmit = () => {
     if(city != ''){
         getWeather(city);
     }else{
-        
+        document.getElementById('span-alert').innerHTML = 'Insira um valor!';
+        document.getElementById('span-alert').className = 'alert';
     }
 }
 
